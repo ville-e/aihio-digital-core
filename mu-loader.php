48f16ab02279c844e0d6f3892d77bd31e761d9ba
Ville Ekman <58355633+ville-e@users.noreply.github.com>
2025-08-01 18:47:57 +0300

    Update mu-loader.php

--- mu-loader.php
+++ mu-loader.php
@@ -1,16 +1,4 @@
 <?php
-/**
- * Must-Use Loader for Aihio Digital – Core
- * Pakottaa pluginin latautumaan MU-tilassa, vaikka varsinainen päivityslogiikka on normaalissa lisäosassa.
- */
-defined('WPMU_PLUGIN_DIR') || define('WPMU_PLUGIN_DIR', WP_CONTENT_DIR . '/mu-plugins');
-
-$plugin_dir = WPMU_PLUGIN_DIR . '/aihio-digital-core';
-$main_file  = $plugin_dir . '/aihio-digital-core.php';
-
-if (file_exists($main_file)) {
-    require_once $main_file;
-} else {
-    // Kehittäjähuomio: asenna plugin kansioon wp-content/mu-plugins/aihio-digital-core/
-    error_log('[Aihio Digital – Core] Main file not found: ' . $main_file);
-}
+// Laita tämä tiedosto suoraan wp-content/mu-plugins -kansioon.
+$main = WPMU_PLUGIN_DIR . '/aihio-digital-core/aihio-digital-core.php';
+if (file_exists($main)) require_once $main;
